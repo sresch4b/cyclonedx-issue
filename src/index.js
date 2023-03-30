fac4dd03139c6c7436e1d9965467e7fb4a33c0f8
Stefan Resch <stefan.resch@bosch.io>
2023-03-30 17:15:43 +0200

    Adds luxon

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
+import {DateTime} from "luxon";
 
 function component() {
   const client = new ApolloClient({
     uri: 'https://example.com/',
     cache: new InMemoryCache(),
   });
+  const now = DateTime.now();
   client.query({query: ''}).then(() => console.log('test'));
   const element = document.createElement('div');
+  element.textContent = now.toISODate();
   return element;
 }
 
